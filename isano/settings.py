5661c12f811d90b26e2944cbe57a009b10b940f3
ahorukomeye patrick <ahorupa@gmail.com>
2021-11-11 12:22:03 +0100

    instal

--- isano/settings.py
+++ isano/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,16 +77,9 @@ WSGI_APPLICATION = 'isano.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fodjango',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-    }
+    'default': dj_database_url.config()
 }
+
 
 
 # Password validation
